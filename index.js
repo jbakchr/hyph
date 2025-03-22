1a0c9102fb513cdfaf3854758ea35c2494f85ed4
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-03-22 19:52:09 +0100

    Added check for folder exists

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
 const fs = require("node:fs");
+const path = require("node:path");
 const { program } = require("commander");
 
 program.argument("<string...>", "String to create dir for").action((str) => {
-  const dirname = str.join("-");
-  fs.mkdirSync(dirname);
+  const dirname = path.join(process.cwd(), str.join("-"));
+
+  if (fs.existsSync(dirname)) {
+    console.log("Folder already exists");
+  } else {
+    fs.mkdirSync(dirname);
+  }
 });
 
 program.parse();
